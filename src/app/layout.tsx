1beeffcf12f2f4074f563109544568dfa3a66eff
Elmahdi Ratil <elmahdiratildev@gmail.com>
2025-09-14 22:29:14 +0100

    version 1

---
+++ src/app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Manrope } from "next/font/google";
+import "./globals.css";
+
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import NextTopLoader from "nextjs-toploader";
+
+const manrope = Manrope({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  display: "swap",
+});
+
+export const metadata: Metadata = {
+  title: "Zakaria Taberkant | photographer portfolio",
+  description:
+    "Explore the portfolio of Zakaria Taberkant a professional photographer.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={manrope.className}>
+      <body>
+        <NextTopLoader showSpinner={false} color="#fff" />
+        <Header />
+        <main className="min-h-screen">{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
