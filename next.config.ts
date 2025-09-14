688cea977ac00a2b1a8532dfdce4f84f3e4e70ac
Elmahdi Ratil <elmahdiratildev@gmail.com>
2025-09-15 00:08:04 +0100

    update

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  
+};
+
+module.exports = {
+  experimental: {
+    turbo: {
+      rules: {}, // optional if you want turbo customization
+    },
+  },
 };
 
 export default nextConfig;
