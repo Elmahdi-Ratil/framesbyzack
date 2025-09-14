02f02a0534e4e8b027b58ed8fc8f676b5f651092
Elmahdi Ratil <elmahdiratildev@gmail.com>
2025-09-14 23:58:03 +0100

    update

--- src/app/about/sections/BioSec.tsx
+++ src/app/about/sections/BioSec.tsx
@@ -17,18 +17,19 @@ const BioSec = () => {
       >
         <motion.h2 variants={fadeInUp}>My Biography</motion.h2>
         <motion.p variants={fadeInUp} className="mt-[30px]">
-          Zakaria Taberkant&apos;s love affair with photography began at a young age,
-  nurtured by the captivating landscapes and vibrant cultures surrounding her
-  in the heart of the MAR. Her passion for storytelling through imagery led
-  her to embark on a photography journey that has spanned over 15 years.
-  Driven by an insatiable curiosity to explore the beauty in everyday moments,
-  Zakaria Taberkant has honed her craft meticulously. Her background in digital
-  media provided her with a solid foundation, but it&apos;s her keen eye for detail
-  and an innate ability to capture raw emotions that truly set her apart.
-  Zakaria Taberkant&apos;s journey is more than just taking pictures; it&apos;s about
-  capturing the essence of the human spirit, the fleeting magic of nature,
-  and the emotions that define our lives. With each click of her camera, she
-  weaves stories that transcend time and space.
+          Zakaria Taberkant love affair with photography began at a young age,
+          nurtured by the captivating landscapes and vibrant cultures
+          surrounding her in the heart of the MAR. Her passion for storytelling
+          through imagery led her to embark on a photography journey that has
+          spanned over 15 years. Driven by an insatiable curiosity to explore
+          the beauty in everyday moments, Zakaria Taberkant has honed her craft
+          meticulously. Her background in digital media provided her with a
+          solid foundation, but it's her keen eye for detail and an innate
+          ability to capture raw emotions that truly set her apart. Zakaria Taberkant
+          journey is more than just taking pictures; it's about capturing the
+          essence of the human spirit, the fleeting magic of nature, and the
+          emotions that define our lives. With each click of her camera, she
+          weaves stories that transcend time and space.
         </motion.p>
       </motion.div>
     </section>
