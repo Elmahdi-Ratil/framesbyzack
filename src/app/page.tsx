8aa008ec08656e3ac97383cb0fa267a33998c6a1
Elmahdi Ratil <elmahdiratildev@gmail.com>
2025-09-24 01:07:22 +0100

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import React from "react";
 import HeroSec from "./home-sections/HeroSec";
 import AboutSec from "./home-sections/AboutSec";
@@ -6,23 +5,36 @@ import ServicesSec from "./home-sections/ServicesSec";
 import PortfolioSec from "./home-sections/PortfolioSec";
 import FaqSec from "@/components/FaqSec";
 import TestimonialsCard from "@/components/TestimonialsCard";
+import Brands from "./portfolio/section/Brands";
 
+{
+  /* HOME section */
+}
 const Home = () => {
   return (
     <>
+      {/* Hero section */}
       <HeroSec />
+      {/* About section */}
       <AboutSec />
+      {/* Services section */}
       <ServicesSec />
+      {/* Portfolio section */}
       <PortfolioSec />
+
+      {/* Components section */}
       {/* FAQ section */}
       <section className="section">
         <FaqSec />
       </section>
-
       {/* Testimonials section */}
       <section className="section">
         <TestimonialsCard />
       </section>
+
+      {/* portfolio section */}
+      {/* Brands section */}
+      <Brands />
     </>
   );
 };
